24361349241ccb1d09fde6ac3b547425046fb43e
Alasdair Haigh <alasdair.haigh@gmail.com>
2016-09-21 22:20:10 +0100

    Added pug template

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var express = require("express");
 var app = express();
 var port = 3700;
 
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "pug");
+app.engine('pug', require('pug').__express);
 app.get("/", function(req, res) {
-  res.send("It works!");
+  res.render("page");
 });
 
+app.use(express.static(__dirname + '/public'));
 app.listen(port);
 console.log("Listening on port " + port);
